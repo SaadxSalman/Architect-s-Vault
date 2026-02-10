0145c89302da06f5694d7c5a9385650e3e3a7258
saadsalmanakram <154062428+SaadxSalman@users.noreply.github.com>
2026-02-11 01:11:24 +0500

    Create Navbar.tsx

---
+++ InvenTrack/frontend/components/Navbar.tsx
@@ -0,0 +1,10 @@
+// components/Navbar.tsx
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4 shadow-lg">
+      <div className="container mx-auto font-bold text-xl">
+        📦 saadxsalman Inventory
+      </div>
+    </nav>
+  );
+}
