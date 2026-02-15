54be790e3e8c44c6b74472471efd89765eee4a80
saadsalmanakram <154062428+SaadxSalman@users.noreply.github.com>
2026-02-15 20:15:49 +0500

    update

--- github-health-dashboard/backend/analytics/apps.py
+++ github-health-dashboard/backend/analytics/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TrackerConfig(AppConfig):
+class AnalyticsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tracker'
+    name = 'analytics'
